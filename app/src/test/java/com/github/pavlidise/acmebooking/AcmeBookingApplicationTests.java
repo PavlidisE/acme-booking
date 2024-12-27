460acc6f632dbdf7ea42e36f9d8ec96b9c7a1eb6
PavlidisNetInt <Evripidis.PAVLIDIS@netcompany-intrasoft.com>
2024-12-27 02:36:55 +0200

    init app skeleton, db room and user ddl, and docker-compose.yaml v0.1

---
+++ app/src/test/java/com/github/pavlidise/acmebooking/AcmeBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pavlidise.acmebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
